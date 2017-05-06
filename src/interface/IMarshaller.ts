c7932765da30974bc5698ec89430e9593e7c3119
wessberg <frederikwessberg@gmail.com>
2017-05-06 11:55:21 +0200

    Added the 'getTypeOf' method to the interface

--- src/interface/IMarshaller.ts
+++ src/interface/IMarshaller.ts
@@ -1,7 +1,9 @@
+import {TypeOf} from '@wessberg/typedetector';
 export declare type ConstructorArgument = any;
 export declare type NewableGeneric<U> = new (...args: ConstructorArgument[]) => U;
 export declare type Newable<U> = NewableGeneric<U>|StringConstructor|NumberConstructor|BooleanConstructor|FunctionConstructor|ObjectConstructor|ArrayConstructor|SetConstructor|SymbolConstructor;
 
 export interface IMarshaller {
 	marshal<T, U> (data: T, hint?: U): U|null|undefined;
+	getTypeOf<T> (data: T): TypeOf;
 }
